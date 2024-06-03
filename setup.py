31e84c040e94e2653f007bf115db050618c18f7e
Lupin-oomura <lupin.oomura@gmail.com>
2024-06-03 14:10:03 +0900

    change setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='mywhisper',
-    version='0.7.1',
+    version='0.7.2',
     packages=find_packages(),
     install_requires=[
         'pydub',
         'speechrecognition',
         'torch',
         'numpy',
-        'pyaudio',
+        # 'pyaudio',
     ],
     url='https://github.com/lupin-oomura/mywhisper.git',
     author='Shin Oomura',
